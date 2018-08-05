930fa8e846f3ba01424af8b735e26aec3be8a534
Raul Chedrese <raulchedrese@gmail.com>
2018-08-04 18:55:34 -0600

    Update ampm formatter to delegate to new formatter in core.

--- src/formatters/ampm.js
+++ src/formatters/ampm.js
@@ -1,3 +1,5 @@
+import parse from "../core/AMPMParser";
+
 (function($) {
   $.timeAutocomplete.formatters.ampm = function() {
     this.initialize.apply(this, arguments);
@@ -362,87 +364,7 @@
     },
 
     parseTime: function(time, format) {
-      var hour,
-        minute,
-        format = format || "H:i:s",
-        pm = time.match(/p/i) !== null,
-        am = time.match(/a/i) !== null,
-        num = time.replace(/[^0-9]/g, "");
-
-      // Parse for hour and minute
-      switch (num.length) {
-        case 4:
-          hour = parseInt(num.charAt(0) + num.charAt(1), 10);
-          minute = parseInt(num.charAt(2) + num.charAt(3), 10);
-          break;
-        case 3:
-          hour = parseInt(num.charAt(0), 10);
-          minute = parseInt(num.charAt(1) + num.charAt(2), 10);
-          break;
-        case 2:
-        case 1:
-          hour = parseInt(num.charAt(0) + (num.charAt(1) || ""), 10);
-          minute = 0;
-          break;
-        default:
-          return "";
-      }
-
-      // if 12 and am/pm not specified assume pm
-      if (hour == 12 && pm === false && am === false) {
-        hour = 12;
-        pm = true;
-      } else if (hour == 12 && pm === false) {
-        hour = 0;
-      } else if (pm === true && hour > 0 && hour < 12) {
-        hour += 12;
-      }
-
-      // Keep within range
-      if (hour <= 0) {
-        hour = 0;
-      }
-
-      if (hour >= 24 && ("" + hour + "").length == 2) {
-        var parts = ("" + hour + "").split("");
-        hour = parseInt(parts[0], 10);
-        minute = parseInt(parts[1], 10);
-        if (minute < 6) {
-          minute = minute + "0";
-        }
-      }
-
-      if (minute < 0 || minute > 59) {
-        minute = 0;
-      }
-
-      if (hour >= 13 && hour <= 23) {
-        pm = true;
-      }
-
-      return (
-        format
-          // 12 hour without leading 0
-          .replace(/g/g, hour === 0 ? "12" : "g")
-          .replace(/g/g, hour > 12 ? hour - 12 : hour)
-          // 12 hour with leading 0
-          .replace(
-            /h/g,
-            hour.toString().length > 1
-              ? hour > 12
-                ? hour - 12
-                : hour
-              : "0" + (hour > 12 ? hour - 12 : hour)
-          )
-          // 24 hour with leading 0
-          .replace(/H/g, hour.toString().length > 1 ? hour : "0" + hour)
-          // minutes with leading zero
-          .replace(/i/g, minute.toString().length > 1 ? minute : "0" + minute)
-          // simulate seconds
-          .replace(/s/g, "00")
-          // lowercase am/pm
-          .replace(/A/g, pm ? this.options.pm_text : this.options.am_text)
-      );
+      return parse(time, format, this.options);
     }
   };
 })(jQuery);
