2d940774ceeb4d7381d1f71b955d34fd13d9b154
Raul Chedrese <raulchedrese@gmail.com>
2018-08-04 18:55:08 -0600

    Configure rollup to use babel.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
-import { uglify } from 'rollup-plugin-uglify';
+import { uglify } from "rollup-plugin-uglify";
+import babel from "rollup-plugin-babel";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    file: 'jquery.timeAutocomplete.min.js',
-    format: 'iife',
+    file: "jquery.timeAutocomplete.min.js",
+    format: "iife"
   },
   plugins: [
-    uglify()
+    uglify(),
+    babel({
+      exclude: "node_modules/**" // only transpile our source code
+    })
   ]
 };
