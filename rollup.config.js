2cd1b1a627e6bedbb7ef1068d3a560927e694d6c
Raul Chedrese <raulchedrese@gmail.com>
2019-08-21 14:50:46 -0600

    Updated build process to use terser and produce an ES module.

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,46 @@
-import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
 
-export default {
-  input: "src/index.js",
-  output: {
-    file: "jquery.timeAutocomplete.min.js",
-    format: "iife"
+export default [
+  {
+    input: "src/index.js",
+    output: {
+      file: "jquery.timeAutocomplete.min.js",
+      format: "iife"
+    },
+    plugins: [
+      terser(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      })
+    ]
   },
-  plugins: [
-    uglify(),
-    babel({
-      exclude: "node_modules/**" // only transpile our source code
-    })
-  ]
-};
+  {
+    input: "src/core/AMPMParser.js",
+    output: {
+      file: "ampm_parser.js",
+      name: "ampm_parser",
+      format: "umd"
+    },
+    plugins: [
+      terser(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      })
+    ]
+  },
+  {
+    input: "src/core/AMPMParser.js",
+    output: {
+      file: "es/ampm_parser.js",
+      name: "ampm_parser",
+      format: "esm"
+    },
+    plugins: [
+      terser(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      })
+    ]
+  }
+];
